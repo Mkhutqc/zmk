6623ebc47c0f4ad977147159da002ff2a31fb464
Mkhutqc <mrhutqc@gmail.com>
2026-03-16 11:31:49 -0400

    Update layer_reporter.c

--- app/src/layer_reporter.c
+++ app/src/layer_reporter.c
@@ -1,6 +1,7 @@
 #include <zephyr/kernel.h>
 #include <zmk/event_manager.h>
 #include <zmk/events/layer_state_changed.h>
+#include <zmk/events/position_state_changed.h>
 #include <zmk/keymap.h>
 
 static int layer_listener(const zmk_event_t *eh) {
@@ -11,5 +12,16 @@ static int layer_listener(const zmk_event_t *eh) {
     return ZMK_EV_EVENT_BUBBLE;
 }
 
+static int position_listener(const zmk_event_t *eh) {
+    const struct zmk_position_state_changed *ev = as_zmk_position_state_changed(eh);
+    if (ev) {
+        printk("ZMK_KEY_%s:%d\n", ev->state ? "DOWN" : "UP", ev->position);
+    }
+    return ZMK_EV_EVENT_BUBBLE;
+}
+
 ZMK_LISTENER(layer_reporter, layer_listener);
 ZMK_SUBSCRIPTION(layer_reporter, zmk_layer_state_changed);
+
+ZMK_LISTENER(position_reporter, position_listener);
+ZMK_SUBSCRIPTION(position_reporter, zmk_position_state_changed);
